84d19ccf6b9225c0687f4cb28769792bdd0eee14
yasminalysha555 <104385154@students.swinburne.edu.my>
2025-10-03 09:19:17 +0800

    feat: reset body; add h1/div/svg; link D3 and energy-d3.js; D3 — style h1, append paragraph, add SVG rects;

---
+++ energy-d3.js
@@ -0,0 +1,14 @@
+d3.select("h1")
+    .style("color", "green");
+d3.select("div")
+    .append("p")
+    .text("Purchasing a low energy consumption TV will help with your energy bills!");
+d3.select("svg")
+    .append("rect")
+d3.select("svg")
+    .append("rect")
+    .attr("x", 50)
+    .attr("y", 50)
+    .attr("width", 100)
+    .attr("height", 30)
+    .style("fill", "green");
